202f7a3821026300ff926b212428cba40e51601a
Abdellah Ramadan <ramadanabdel24@gmail.com>
2025-02-27 19:25:52 +0100

    refactor little configurations

---
+++ config/definition.php
@@ -0,0 +1,22 @@
+<?php
+
+use Symfony\Component\Config\Definition\Configurator\DefinitionConfigurator;
+
+return static function (DefinitionConfigurator $definition) {
+    $definition->rootNode()
+        ->children()
+            ->arrayNode('schema')
+                ->children()
+
+                ->end()
+            ->end()
+            ->arrayNode('meta_tags')
+                ->children()
+                    ->scalarNode('title')->end()
+                    ->scalarNode('description')->end()
+                    ->arrayNode('keywords')
+                    ->info('Meta keywords')->example('meta, keyword')
+                ->end()
+            ->end()
+        ->end();
+};
