cf710ae92edd1e8f935502bdf006c780ce186736
Dustin Meiner <meiner@slis-services.de>
2025-12-14 22:37:43 +0100

    Add address property and method to Place class for improved address handling

--- src/Schema/Thing/Place.php
+++ src/Schema/Thing/Place.php
@@ -6,5 +6,11 @@ use Rami\SeoBundle\Schema\BaseType;
 
 class Place extends BaseType
 {
+    public array $properties = [];
 
+    public function address(string $address): static
+    {
+        $this->setProperty('address', $address);
+        return $this;
+    }
 }
