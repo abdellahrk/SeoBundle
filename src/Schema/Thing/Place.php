f48bdab0598f6dc8cc4a538ba05f47ec2a2f8039
Dustin Meiner <meiner@slis-services.de>
2025-12-14 22:42:30 +0100

    Update address method in Place class to accept PostalAddress type for improved type safety

--- src/Schema/Thing/Place.php
+++ src/Schema/Thing/Place.php
@@ -3,14 +3,15 @@
 namespace Rami\SeoBundle\Schema\Thing;
 
 use Rami\SeoBundle\Schema\BaseType;
+use Rami\SeoBundle\Schema\Intangible\StructuredValue\ContactPoint\PostalAddress;
 
 class Place extends BaseType
 {
     public array $properties = [];
 
-    public function address(string $address): static
+    public function address(PostalAddress $address): static
     {
-        $this->setProperty('address', $address);
+        $this->setProperty('address', $this->parseChild($address));
         return $this;
     }
 }
