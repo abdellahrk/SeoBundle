12385ecc1a1b7cb566f4f3cc8233545f7557190b
Dustin Meiner <meiner@slis-services.de>
2025-12-15 18:36:20 +0100

    Refactor BlogPosting class to use multiple traits for enhanced functionality and structure

--- src/Schema/Thing/CreativeWork/Article/SocialMediaPosting/BlogPosting.php
+++ src/Schema/Thing/CreativeWork/Article/SocialMediaPosting/BlogPosting.php
@@ -3,7 +3,15 @@
 namespace Rami\SeoBundle\Schema\Thing\CreativeWork\Article\SocialMediaPosting;
 
 use Rami\SeoBundle\Schema\BaseType;
+use Rami\SeoBundle\Schema\Traits\ArticleTrait;
+use Rami\SeoBundle\Schema\Traits\CreativeWorkTrait;
+use Rami\SeoBundle\Schema\Traits\SocialMediaPostingTrait;
+use Rami\SeoBundle\Schema\Traits\ThingTrait;
 
 class BlogPosting extends BaseType
 {
+    use ThingTrait;
+    use CreativeWorkTrait;
+    use ArticleTrait;
+    use SocialMediaPostingTrait;
 }
