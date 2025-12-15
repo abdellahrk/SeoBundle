08c5b8d619cd3f1c36e80c535d9c8375ddd9bc13
Dustin Meiner <meiner@slis-services.de>
2025-12-15 18:38:08 +0100

    Refactor WebPage class to extend BaseType and implement traits for improved structure and functionality

--- src/Schema/Thing/CreativeWork/WebPage.php
+++ src/Schema/Thing/CreativeWork/WebPage.php
@@ -2,10 +2,14 @@
 
 namespace Rami\SeoBundle\Schema\Thing\CreativeWork;
 
-use Rami\SeoBundle\Schema\Thing\CreativeWork;
+use Rami\SeoBundle\Schema\BaseType;
+use Rami\SeoBundle\Schema\Traits\CreativeWorkTrait;
+use Rami\SeoBundle\Schema\Traits\ThingTrait;
 use Rami\SeoBundle\Schema\Traits\WebPageTrait;
 
-class WebPage extends CreativeWork
+class WebPage extends BaseType
 {
+    use ThingTrait;
+    use CreativeWorkTrait;
     use WebPageTrait;
 }
