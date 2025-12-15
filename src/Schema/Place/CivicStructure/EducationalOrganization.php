aa117f788e57dcc02ccc94c4820d7a157d054be2
Dustin Meiner <meiner@slis-services.de>
2025-12-15 20:18:49 +0100

    Refactor EducationalOrganization class to extend Organization and remove BaseType

--- src/Schema/Place/CivicStructure/EducationalOrganization.php
+++ src/Schema/Place/CivicStructure/EducationalOrganization.php
@@ -3,9 +3,10 @@
 namespace Rami\SeoBundle\Schema\Place\CivicStructure;
 
 use Rami\SeoBundle\Schema\BaseType;
+use Rami\SeoBundle\Schema\Thing\Organization;
 use Rami\SeoBundle\Schema\Thing\Person;
 
-class EducationalOrganization extends BaseType
+class EducationalOrganization extends Organization
 {
     public function alumni(Person $alumni): static
     {
