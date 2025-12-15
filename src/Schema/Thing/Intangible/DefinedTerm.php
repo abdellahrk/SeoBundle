ffe784a39bca613f389014a662f1b8d21bf47ecc
Dustin Meiner <meiner@slis-services.de>
2025-12-15 20:22:41 +0100

    Refactor DefinedTerm class to extend Thing and add termCode property setter

--- src/Schema/Thing/Intangible/DefinedTerm.php
+++ src/Schema/Thing/Intangible/DefinedTerm.php
@@ -2,9 +2,13 @@
 
 namespace Rami\SeoBundle\Schema\Thing\Intangible;
 
-use Rami\SeoBundle\Schema\BaseType;
+use Rami\SeoBundle\Schema\Thing;
 
-class DefinedTerm extends BaseType
+class DefinedTerm extends Thing
 {
-
+    public function termCode(string $code): static
+    {
+        $this->setProperty('termCode', $code);
+        return $this;
+    }
 }
