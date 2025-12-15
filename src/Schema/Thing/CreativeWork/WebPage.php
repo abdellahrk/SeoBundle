eaeb98d5c0da65e06fb1698ccc553f8af707749a
Dustin Meiner <meiner@slis-services.de>
2025-12-15 18:49:13 +0100

    Refactor WebPage class to extend CreativeWork for improved structure

--- src/Schema/Thing/CreativeWork/WebPage.php
+++ src/Schema/Thing/CreativeWork/WebPage.php
@@ -2,14 +2,10 @@
 
 namespace Rami\SeoBundle\Schema\Thing\CreativeWork;
 
-use Rami\SeoBundle\Schema\BaseType;
-use Rami\SeoBundle\Schema\Traits\CreativeWorkTrait;
-use Rami\SeoBundle\Schema\Traits\ThingTrait;
+use Rami\SeoBundle\Schema\Thing\CreativeWork;
 use Rami\SeoBundle\Schema\Traits\WebPageTrait;
 
-class WebPage extends BaseType
+class WebPage extends CreativeWork
 {
-    use ThingTrait;
-    use CreativeWorkTrait;
     use WebPageTrait;
 }
