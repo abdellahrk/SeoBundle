00961ea66ca6b07155c7008d5c2a21bb6ecec76b
Dustin Meiner <meiner@slis-services.de>
2025-12-15 17:43:24 +0100

    Remove unused imports in Thing class for cleaner code

--- src/Schema/Thing.php
+++ src/Schema/Thing.php
@@ -2,14 +2,6 @@
 
 namespace Rami\SeoBundle\Schema;
 
-use Rami\SeoBundle\Schema\Intangible\Service;
-use Rami\SeoBundle\Schema\Thing\Action;
-use Rami\SeoBundle\Schema\Thing\CreativeWork;
-use Rami\SeoBundle\Schema\Thing\CreativeWork\ImageObject;
-use Rami\SeoBundle\Schema\Thing\CreativeWork\TextObject;
-use Rami\SeoBundle\Schema\Thing\Intangible\StructuredValue\PropertyValue;
-use Rami\SeoBundle\Schema\Thing\Organization;
-use Rami\SeoBundle\Schema\Thing\Person;
 use Rami\SeoBundle\Schema\Traits\ThingsTrait;
 
 class Thing extends BaseType
