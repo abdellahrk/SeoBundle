58fc39546e20d51d7949a9fab65714fcb02294fa
Dustin Meiner <meiner@slis-services.de>
2025-12-15 19:21:29 +0100

    Refactor TextObject class to extend MediaObject and remove unused namespace

--- src/Schema/Thing/CreativeWork/TextObject.php
+++ src/Schema/Thing/CreativeWork/TextObject.php
@@ -2,8 +2,6 @@
 
 namespace Rami\SeoBundle\Schema\Thing\CreativeWork;
 
-use Rami\SeoBundle\Schema\Thing\CreativeWork;
-
-class TextObject extends CreativeWork
+class TextObject extends MediaObject
 {
 }
