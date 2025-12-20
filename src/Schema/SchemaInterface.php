5003d5b7da8859e7a1a3e8fb1b254c482da1ad77
Dustin Meiner <meiner@slis-services.de>
2025-12-20 22:49:54 +0100

    Enhance SchemaInterface by adding new methods and fully qualified class names for improved structure and clarity

--- src/Schema/SchemaInterface.php
+++ src/Schema/SchemaInterface.php
@@ -15,9 +15,13 @@ use Rami\SeoBundle\Schema\Thing\CreativeWork;
 use Rami\SeoBundle\Schema\Thing\CreativeWork\Article;
 use Rami\SeoBundle\Schema\Thing\CreativeWork\Article\SocialMediaPosting;
 use Rami\SeoBundle\Schema\Thing\CreativeWork\Article\SocialMediaPosting\BlogPosting;
+use Rami\SeoBundle\Schema\Thing\CreativeWork\Blog;
+use Rami\SeoBundle\Schema\Thing\CreativeWork\Course;
+use Rami\SeoBundle\Schema\Thing\CreativeWork\EducationalOccupationalCredential;
 use Rami\SeoBundle\Schema\Thing\CreativeWork\MediaObject;
 use Rami\SeoBundle\Schema\Thing\CreativeWork\MediaObject\ImageObject;
 use Rami\SeoBundle\Schema\Thing\CreativeWork\MediaObject\TextObject;
+use Rami\SeoBundle\Schema\Thing\CreativeWork\Thesis;
 use Rami\SeoBundle\Schema\Thing\CreativeWork\WebPage;
 use Rami\SeoBundle\Schema\Thing\CreativeWork\WebPage\AboutPage;
 use Rami\SeoBundle\Schema\Thing\CreativeWork\WebPage\CheckoutPage;
@@ -30,13 +34,25 @@ use Rami\SeoBundle\Schema\Thing\CreativeWork\WebPage\ItemPage;
 use Rami\SeoBundle\Schema\Thing\CreativeWork\WebPage\ProfilePage;
 use Rami\SeoBundle\Schema\Thing\CreativeWork\WebPage\SearchResultsPage;
 use Rami\SeoBundle\Schema\Thing\CreativeWork\WebPageElement;
+use Rami\SeoBundle\Schema\Thing\CreativeWork\WebSite;
 use Rami\SeoBundle\Schema\Thing\Event;
 use Rami\SeoBundle\Schema\Thing\Intangible\BreadcrumbList;
+use Rami\SeoBundle\Schema\Thing\Intangible\DefinedTerm;
+use Rami\SeoBundle\Schema\Thing\Intangible\JobPosting;
 use Rami\SeoBundle\Schema\Thing\Intangible\ListItem;
+use Rami\SeoBundle\Schema\Thing\Intangible\Occupation;
 use Rami\SeoBundle\Schema\Thing\Intangible\Specialty;
+use Rami\SeoBundle\Schema\Thing\Intangible\StructuredValue\MonetaryAmount;
 use Rami\SeoBundle\Schema\Thing\Intangible\StructuredValue\PropertyValue;
 use Rami\SeoBundle\Schema\Thing\Intangible\StructuredValue\SpeakableSpecification;
+use Rami\SeoBundle\Schema\Thing\Organization;
+use Rami\SeoBundle\Schema\Thing\Organization\LocalBusiness;
+use Rami\SeoBundle\Schema\Thing\Organization\LocalBusiness\Library;
+use Rami\SeoBundle\Schema\Thing\Organization\LocalBusiness\RadioStation;
+use Rami\SeoBundle\Schema\Thing\Organization\LocalBusiness\TravelAgency;
 use Rami\SeoBundle\Schema\Thing\Person;
+use Rami\SeoBundle\Schema\Thing\Place;
+use Rami\SeoBundle\Schema\Thing\Place\AdministrativeArea\Country;
 
 interface SchemaInterface
 {
@@ -45,6 +61,8 @@ interface SchemaInterface
     public function thing(): Thing;
 
     public function service(): Service;
+    public function website(): WebSite;
+
 
     public function creativeWork(): CreativeWork;
 
@@ -58,7 +76,37 @@ interface SchemaInterface
 
     public function audience(): Audience;
 
+    public function jobPosting(): JobPosting;
+
     public function itemList(): ItemList;
+
+    public function occupation(): Occupation;
+
+    public function monetaryAmount(): MonetaryAmount;
+
+    public function educationOccupationalCredential(): EducationalOccupationalCredential;
+
+    public function definedTerm(): DefinedTerm;
+
+    public function country(): Country;
+
+    public function place(): Place;
+
+    public function localBusiness(): LocalBusiness;
+
+    public function library(): Library;
+
+    public function radioStation(): RadioStation;
+
+    public function travelAgency(): TravelAgency;
+
+    public function blog(): Blog;
+
+    public function course(): Course;
+
+    public function thesis(): Thesis;
+
+    public function organization(): Organization;
 
     public function webPage(): WebPage;
 
